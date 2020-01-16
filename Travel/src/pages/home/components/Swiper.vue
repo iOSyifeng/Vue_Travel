6085a65ffbdf342f18934eaad81877e445416dd7
逸枫 <15757126126@sina.cn>
2020-01-16 10:45:58 +0800

    轮播图

---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/613d54428519648cba238f8cdcf681f1.jpg_750x200_17612e62.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/7b45da191f638f13455777030853a1ea.jpg_750x200_883fe3a8.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff !important
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 26.6%
+  background: #eee
+  .swiper-img
+    width: 100%
+
+</style>
