fd307b2344acde1894a7232b5dc2c78d02216714
逸枫 <15757126126@sina.cn>
2020-01-17 09:44:46 +0800

    add icons

---
+++ Travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src='item.imgUrl'/>
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                 </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/b8/c5dcdb58deec2402.png',
+        desc: '西溪周家村'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '杭州野生'
+      }, {
+        id: '0004',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/f5/a963333e1fa802.png',
+        desc: '本地玩乐'
+      }, {
+        id: '0005',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '0006',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '滑雪'
+      }, {
+        id: '0007',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
+        desc: '千岛湖'
+      }, {
+        id: '0008',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/c1/6f15f887179fa002.png',
+        desc: '灵隐寺'
+      }, {
+        id: '0009',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+        desc: '杭州宋城'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    @import '~styles/mixins.styl'
+    .icons >>> .swiper-container
+        height: 0
+        padding-bottom: 50%
+    .icon
+        position: relative
+        overflow: hidden
+        float: left
+        width: 25%
+        height: 0
+        padding-bottom: 25%
+        .icon-img
+            position: absolute
+            top: 0
+            left: 0
+            right: 0
+            bottom: .44rem
+            box-sizing: border-box
+            padding: .1rem
+            .icon-img-content
+                display: block
+                margin: 0 auto
+                height: 100%
+        .icon-desc
+            position: absolute
+            left: 0
+            right: 0
+            bottom: 0
+            height: .44rem
+            line-height: .44rem
+            text-align: center
+            color: $darkTextColor
+            ellipsis()
+
+</style>
