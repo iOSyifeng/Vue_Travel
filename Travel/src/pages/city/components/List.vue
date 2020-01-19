5fc3bd91ede1821a67f32646f2cb7ca14c778217
逸枫 <15757126126@sina.cn>
2020-01-19 11:30:30 +0800

    finish city-list

--- Travel/src/pages/city/components/List.vue
+++ Travel/src/pages/city/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="list" ref="warpper">
+    <div class="list" ref="wrapper">
         <div>
             <div class="area">
                 <div class="title border-topbottom">当前城市</div>
@@ -11,12 +11,17 @@
             </div>
             <div class="area">
                 <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper" v-for="item of hot" :key="item.id">
+                        <div class="button">{{item.name}}</div>
+                    </div>
+                </div>
             </div>
-            <div class="area">
-                <div class="title border-topbottom">A</div>
+            <div class="area" v-for="(item, key) of cities" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
+                    <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+                        {{innerItem.name}}
                     </div>
                 </div>
             </div>
@@ -28,6 +33,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
